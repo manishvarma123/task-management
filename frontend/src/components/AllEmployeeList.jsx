8e905dbde0c2d533cdbf8170ab159448aaa0bf23
manishvarma123 <manishvarma1302@gmail.com>
2025-03-22 23:40:47 +0530

    all employee list updated

--- frontend/src/components/AllEmployeeList.jsx
+++ frontend/src/components/AllEmployeeList.jsx
@@ -13,8 +13,29 @@ const AllEmployeeList = () => {
     const [allUser, setAllUser] = useState(null)
     const navigate = useNavigate()
     const [loading, setLoading] = useState(false)
+    const [searchQuery, setSearchQuery] = useState("");
     const { user } = useSelector((state) => state.user);
-    
+    const [page, setPage] = useState(1); // Page starts at 1
+    const [rows, setRows] = useState(6); // Rows per page
+    const [totalPages, setTotalPages] = useState(1); // Total pages
+
+    const handleSearch = (e) => {
+        const query = e.target.value.toLowerCase();
+        setSearchQuery(query);
+    };
+
+    const handlePrevious = () => {
+        if (page > 1) {
+            setPage(page - 1); // Decrease page number
+        }
+    };
+
+    const handleNext = () => {
+        if (page < totalPages) {
+            setPage(page + 1); // Increase page number
+        }
+    };
+
     useEffect(() => {
 
         const fetchAllUser = async () => {
@@ -27,7 +48,11 @@ const AllEmployeeList = () => {
                 //     withCredentials: true
                 // })
 
-                const res = await api.getAllUser()
+                const res = await api.getAllUser({
+                    page: page,
+                    limit: rows,
+                    searchQuery: searchQuery,
+                })
                 setAllUser(res?.data?.data.allUser)
 
             } catch (error) {
@@ -42,13 +67,14 @@ const AllEmployeeList = () => {
         }
 
         fetchAllUser()
-    }, [])
+    }, [page, rows, searchQuery])
 
     if (loading) {
         return (
             <div className="w-full h-full p-4">
-                <h2 className="font-semibold text-xl border-b-2 border-slate-100 pb-2 mb-4">
+                <h2 className="font-semibold text-xl border-b-2 border-slate-100 pb-2 mb-4 ">
                     All Employee List
+
                 </h2>
                 <h1 className='text-center text-base'>
                     Loading...
@@ -58,8 +84,14 @@ const AllEmployeeList = () => {
     }
     return (
         <div className="w-full h-full p-4">
-            <h2 className="font-semibold text-xl border-b-2 border-slate-100 pb-2 mb-4">
+            <h2 className="font-semibold text-xl border-b-2 border-slate-100 pb-2 mb-4 flex justify-between items-center">
                 All Employee List
+                <input
+                    value={searchQuery}
+                    onChange={handleSearch}
+                    placeholder="Search by name/email"
+                    className="text-sm border-2 border-slate-200 px-3 py-1"
+                />
             </h2>
 
             <div className="w-full overflow-x-auto">
@@ -95,6 +127,14 @@ const AllEmployeeList = () => {
                         }
                     </tbody>
                 </table>
+            </div>
+
+            <div className="flex justify-center items-center gap-3 mt-4 text-center">
+                <div className="flex items-center gap-3">
+                    <button className="bg-slate-100" onClick={handlePrevious} disabled={page <= 1}>Previous</button>
+                    <span>Page {page} of {totalPages}</span>
+                    <button className="bg-slate-100" onClick={handleNext} disabled={page >= totalPages}>Next</button>
+                </div>
             </div>
         </div>
     );
